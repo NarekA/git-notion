110128dccaa4e3cfebd619637cbc951f11996256
Narek <amirbekiann@gmail.com>
2020-12-17 15:31:39 -0800

    Bump version: 0.2.3 → 0.2.4

--- git_notion/__init__.py
+++ git_notion/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for git-notion."""
 
 __author__ = """NarekA"""
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 from git_notion.git_notion import *
--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/NarekA/git-notion',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
