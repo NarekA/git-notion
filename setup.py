6436ef680d2a53504c0d3e0c081fbfaeedf4feb1
Narek Amirbekian <NarekA@users.noreply.github.com>
2022-06-21 11:45:48 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/NarekA/git-notion',
-    version='0.2.4',
+    version='0.2.5',
     zip_safe=False,
 )
